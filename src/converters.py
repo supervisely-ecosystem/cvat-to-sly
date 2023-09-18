bfb857f71f476a032e3125d7c406b19e2d33090e
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-09-18 13:28:27 +0300

    Added polygon converter.

--- src/converters.py
+++ src/converters.py
@@ -14,6 +14,10 @@ def convert_rectangle(cvat_label: Dict[str, str]) -> sly.Label:
     class_name = cvat_label["label"]
     obj_class = sly.ObjClass(name=class_name, geometry_type=sly.Rectangle)
 
+    # XML Example:
+    # <box label="wheel" occluded="0" xtl="220.67" ytl="213.36" xbr="258.34" ybr="249.50" z_order="0">
+    # </box>
+
     sly_label = sly.Label(
         geometry=sly.Rectangle(
             top=int(float(cvat_label["ytl"])),
@@ -27,7 +31,36 @@ def convert_rectangle(cvat_label: Dict[str, str]) -> sly.Label:
     return sly_label
 
 
+def convert_polygon(cvat_label: Dict[str, str]) -> sly.Label:
+    """Converts a label with "polygon" geometry from CVAT format to Supervisely format.
+
+    :param cvat_label: label in CVAT format (from XML parser)
+    :type cvat_label: Dict[str, str]
+    :return: label in Supervisely format
+    :rtype: sly.Label
+    """
+    class_name = cvat_label["label"]
+    obj_class = sly.ObjClass(name=class_name, geometry_type=sly.Polygon)
+
+    # XML Example:
+    # <polygon label="mirror" points="195.68,191.45;199.91,195.29;196.45,201.06;192.22,199.14" z_order="0">
+    # </polygon>
+
+    point_pairs = cvat_label["points"].split(";")
+    exterior = []
+    for point_pair in point_pairs:
+        y, x = point_pair.split(",")
+        exterior.append((int(float(x)), int(float(y))))
+
+    sly_label = sly.Label(
+        geometry=sly.Polygon(exterior=exterior),
+        obj_class=obj_class,
+    )
+
+    return sly_label
+
+
 CONVERT_MAP = {
-    # "polygon": convert_polygon,
+    "polygon": convert_polygon,
     "box": convert_rectangle,
 }
