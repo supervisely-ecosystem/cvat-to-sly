c598ab048591d521148ba4eb4eec74e940e725fe
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-11-10 13:15:45 +0200

    Update globals.py

--- import_cvat/src/globals.py
+++ import_cvat/src/globals.py
@@ -26,7 +26,7 @@ if sly.is_development():
     else:
         sly.logger.warning("One of the .env files is missing. It may cause errors.")
 
-api: sly.Api = sly.Api.from_env()
+api = sly.Api.from_env()
 SLY_APP_DATA_DIR = sly.app.get_data_dir()
 sly.logger.debug(f"App starting... SLY_APP_DATA_DIR: {SLY_APP_DATA_DIR}")
 
